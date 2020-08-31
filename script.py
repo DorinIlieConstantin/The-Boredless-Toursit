027e37865a8c10b96339e8605c828052f033e447
codecademy <ccuser@codecademy.com>
2020-08-31 15:04:05 +0000

    Added test objects

---
+++ script.py
@@ -0,0 +1,6 @@
+destinations=["Paris, France”
+"Shanghai, China”
+"Los Angeles, USA”
+"São Paulo, Brazil”
+"Cairo, Egypt”]
+test_traveler=['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
